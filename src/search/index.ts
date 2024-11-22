c0f0c0e433dc2d9db07993dd5eec2e310ff1bfe5
Sultonov-557 <sultonovzerifboy@gmail.com>
2024-11-22 14:23:45 +0500

    added redis

--- src/search/index.ts
+++ src/search/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { WebPageRepo } from "../database/index";
 import { Or, Raw } from "typeorm";
 import path from "path";
+import { redis } from "../database/redis";
 
 export function start() {
   const app = express();
@@ -13,6 +14,14 @@ export function start() {
   app.get("/search/:query", async (req, res) => {
     const { query } = req.params;
     const { page } = req.query as { page: string };
+
+    const key = `${page||1},${query}`;
+    const cache = await redis.get(key);
+    if (cache) {
+      res.send(JSON.parse(cache));
+      return;
+    }
+
     const limit = 10;
     const offset = (parseInt(page || "1", 10) - 1) * limit;
     const querySplit = query.split(" ");
@@ -65,12 +74,15 @@ export function start() {
     const endTime = new Date();
     const searchTime = endTime.getTime() - startTime.getTime();
 
-    res.send({
+    const output = {
       data,
       time: searchTime,
       current: parseInt(page, 10),
       total: totalPages,
-    });
+    };
+
+    await redis.set(key, JSON.stringify(output));
+    res.send(output);
   });
 
   app.listen(3000, () => console.log(`listening on port 3000`));
