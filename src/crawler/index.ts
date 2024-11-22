47f0445ea088ca947a11ee2c2286da164510d98a
Sultonov-557 <sultonovzerifboy@gmail.com>
2024-11-22 15:35:32 +0500

    changed ids in database to lowercase

--- src/crawler/index.ts
+++ src/crawler/index.ts
@@ -54,37 +54,44 @@ export function start() {
 }
 
 async function next() {
+  setTimeout(next, 1000);
+
   const worker = workers.shift();
   if (!worker) return;
+
+  frame++;
 
   const queue =
     (await QueueRepo.find({ take: 1, where: { url: Not(Like("%en.wikipedia.org%")) } }))[0] ||
     (await QueueRepo.find({ take: 1 }))[0];
+
   if (queue) {
     if (!(await WebPageRepo.existsBy({ url: queue.url }))) {
       console.log(`scraping ${queue.url}`);
       worker.postMessage(queue.url);
+    } else {
+      workers.push(worker);
     }
 
     if (!TEST_MODE) {
       await QueueRepo.delete(queue);
     }
+  } else {
+    workers.push(worker);
   }
 
-  if (frame == 20) {
+  if (frame == 50) {
     frame = 0;
     await database.query(`
       DELETE FROM queue T1
       USING webpage T2
-      WHERE T1.url = T2.url
+      WHERE T1.id>T2.id AND T1.url = T2.url
     `);
     await database.query(`
       DELETE FROM queue T1
       USING queue T2
-      WHERE T1.url = T2.url
+      WHERE T1.id>T2.id AND T1.url = T2.url
     `);
-    console.log("cleaned database from dubs")
+    console.log("cleaned database from dubs");
   }
-
-  setTimeout(next, 1000);
 }
--- src/database/entities/webpage.entity.ts
+++ src/database/entities/webpage.entity.ts
@@ -3,7 +3,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 @Entity()
 export class Webpage {
   @PrimaryGeneratedColumn()
-  ID: number;
+  id: number;
 
   @Column("text")
   url: string;
